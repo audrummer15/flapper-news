a91d679b3567b870ccb3542ff2eea8752348668a
Adam Brown <audrummer15@gmail.com>
2015-06-17 13:49:50 -0500

    Broken

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ require('./models/Comments');
 require('./models/Users');
 require('./config/passport');
 mongoose.connect('mongodb://' + 
-  //process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + 
-  //process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+  process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + 
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + 
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + 
   process.env.OPENSHIFT_APP_NAME);
