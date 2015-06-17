9369a939e78f87ffa46251fa15c1a2d905e32a8b
Adam Brown <audrummer15@gmail.com>
2015-06-17 12:27:17 -0500

    Added app database name

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ require('./models/Posts');
 require('./models/Comments');
 require('./models/Users');
 require('./config/passport');
-mongoose.connect('mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/');
+mongoose.connect('mongodb://' + 
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + 
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + 
+  process.env.OPENSHIFT_APP_NAME);
 
 
 var routes = require('./routes/index');
